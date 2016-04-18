0908c2f632ed7b94047084600588bad03b1d8317
Prokofyev Alexander <prokofyev.alexander@gmail.com>
2016-04-18 11:29:20 +0300

    Redirect to new domain

--- server/index.js
+++ server/index.js
@@ -44,6 +44,7 @@ redirect.use(function (req, res) {
 var vhostApp = express();
 
 vhostApp.use(vhost('*.fastvps-server.com', redirect));
+vhostApp.use(vhost('fastvps-server.com', redirect));
 vhostApp.use(vhost('*.' + config.express.host, redirect));
 vhostApp.use(vhost(config.express.host, app));
 
